29cc4b7e9d7dc02566fe84331967a604665e0ed8
ShariyarAbeer <007abeershariyar@gmail.com>
2021-02-17 05:23:27 +0600

    tast list create

---
+++ lib/screens/list_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListTest extends StatefulWidget {
+  @override
+  _ListTestState createState() => _ListTestState();
+}
+
+class _ListTestState extends State<ListTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
