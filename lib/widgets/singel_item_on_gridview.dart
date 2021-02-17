b93b66a4adfb5078b438d7ccb16795d2ee32d8a9
ShariyarAbeer <007abeershariyar@gmail.com>
2021-02-18 00:56:27 +0600

    more or less UI develop :)

--- lib/widgets/singel_item_on_gridview.dart
+++ lib/widgets/singel_item_on_gridview.dart
@@ -9,31 +9,36 @@ class _SingleItemOnGridviewState extends State<SingleItemOnGridview> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Image.network(
-              "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg"),
-          Container(
-            child: Text("all pricing"),
-          ),
-          Container(
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  Container(
-                    child: Text("this is price"),
-                  ),
-                  Container(
-                    alignment: Alignment.topLeft,
-                    margin: EdgeInsets.all(20),
-                    child: Icon(Icons.shopping_cart),
-                  ),
-                ],
+      elevation: 4,
+      margin: EdgeInsets.all(10),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Image.network(
+                "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg"),
+            Container(
+              child: Text("all pricing"),
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text("this is price"),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: EdgeInsets.only(left: 150),
+                      child: Icon(Icons.shopping_cart),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
