3b662bd78060262299cead42801b3c322931e911
ShariyarAbeer <007abeershariyar@gmail.com>
2021-02-17 22:47:49 +0600

    api data on main page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +9,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var data;
+  List datalist = [];
+
+  Future getdata() async {
+    var response =
+        await http.get("https://jsonplaceholder.typicode.com/photos");
+    setState(() {
+      var decode = jsonDecode(response.body);
+      data = decode;
+      datalist = data;
+      print(datalist);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.getdata();
+  }
+
   final List<Map> myProducts =
       List.generate(100, (index) => {"id": index, "name": "Product $index"})
           .toList();
@@ -15,27 +39,36 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Shopping App"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          itemCount: myProducts.length,
-          itemBuilder: (BuildContext ctx, index) {
-            return Container(
-              alignment: Alignment.center,
-              child: Text(myProducts[index]["name"]),
-              decoration: BoxDecoration(
-                color: Colors.blue[600],
-                borderRadius: BorderRadius.circular(15),
-              ),
-            );
-          },
-        ),
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(data[index]["id"].toString()),
+            subtitle: Text(data[index]["title"]),
+          );
+        },
       ),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(8.0),
+      //   child: GridView.builder(
+      //     gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+      //         maxCrossAxisExtent: 200,
+      //         childAspectRatio: 3 / 2,
+      //         crossAxisSpacing: 20,
+      //         mainAxisSpacing: 20),
+      //     itemCount: myProducts.length,
+      //     itemBuilder: (BuildContext ctx, index) {
+      //       return Container(
+      //         alignment: Alignment.center,
+      //         child: Text(myProducts[index]["name"]),
+      //         decoration: BoxDecoration(
+      //           color: Colors.blue[600],
+      //           borderRadius: BorderRadius.circular(15),
+      //         ),
+      //       );
+      //     },
+      //   ),
+      // ),
     );
   }
 }
